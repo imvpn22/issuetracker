d691f4fbaba604a5b7bdae62cd400bd581a50f2c
Vipin Yadav <imvpn22@gmail.com>
2019-03-18 20:55:07 +0530

    minor fix

--- routes/issues.js
+++ routes/issues.js
@@ -17,10 +17,8 @@ router.get('/', function(req, res) {
   } else {
     IssueController.getAllIssues(function(err, issues) {
       if (err) {
-        console.log(err);
         res.send(err);
       } else {
-        console.log(issues);
         res.json(issues);
       }
     });
