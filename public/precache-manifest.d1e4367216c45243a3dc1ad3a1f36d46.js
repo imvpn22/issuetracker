2671b86438a471a9d9a64e0d0ab895b8247464c5
Vipin Yadav <imvpn22@gmail.com>
2019-04-10 21:26:06 +0530

    Added react build

---
+++ public/precache-manifest.d1e4367216c45243a3dc1ad3a1f36d46.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ad97b0b0e20eb5f443dc",
+    "url": "/static/js/main.57cada24.chunk.js"
+  },
+  {
+    "revision": "b565fa70e0ee77d3ae66",
+    "url": "/static/js/2.0a300ecf.chunk.js"
+  },
+  {
+    "revision": "ad97b0b0e20eb5f443dc",
+    "url": "/static/css/main.99560c3d.chunk.css"
+  },
+  {
+    "revision": "bdaba3e881265f218cf241cc2f1c4a23",
+    "url": "/index.html"
+  }
+];
